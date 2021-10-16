5e2e9b213f46e9dd234b7dd293ad813a6c0525d5
IGOR <kondrahov.2000@mail.ru>
2021-10-16 19:10:23 +0300

    Added icons to character class

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,21 +2,50 @@ import React from 'react';
 import { addToFavorites, removeFromFavorites } from '../actions/charactersActions';
 import { connect } from 'react-redux';
 import './Character.css';
-import { FaStar, FaRegStar } from 'react-icons/fa';
+import { 
+    FaStar, 
+    FaRegStar, 
+    FaVenus,
+    FaMars,
+    FaTransgender,
+    FaGenderless,
+    FaBirthdayCake,
+    FaEye
+} from 'react-icons/fa';
+import { 
+    GiBodyHeight,
+    GiWeight
+} from 'react-icons/gi'
 
 class Character extends React.Component
 {
+    findGender(gender) {
+        switch(gender) {
+            case 'male':
+                return <FaMars />
+            case 'female':
+                return <FaVenus />
+            case 'hermaphrodite':
+                return <FaTransgender />
+            default:
+                return <FaGenderless />
+        }
+    }
+
     render() {
         const { info, isFavorite } = this.props;
+        let genderIcon = this.findGender(info.gender);
         if(info) {
             return (
                 <div className="character">
                     <h2>{info.name ? info.name : "No name provided"}</h2>
-                    <div>Height: {info.height ? info.height : "No height provided"}</div>
-                    <div>Mass: {info.mass ? info.mass : "No mass provided"}</div>
-                    <div>Birth Year: {info.birth_year ? info.birth_year : "No birth year provided"}</div>
-                    <div>Eye Color: {info.eye_color ? info.eye_color : "No eye color provided"}</div>
-                    <div>Gender: {info.gender ? info.gender : "No gender provided"}</div>
+                    <div style={{marginLeft: "auto", textAlign: "left"}}>
+                        <div><GiBodyHeight /> {info.height ? info.height : "No height provided"}</div>
+                        <div><GiWeight /> {info.mass ? info.mass : "No mass provided"}</div>
+                        <div><FaBirthdayCake /> {info.birth_year ? info.birth_year : "No birth year provided"}</div>
+                        <div><FaEye fill={info.eye_color}/> {info.eye_color ? info.eye_color : "No eye color provided"}</div>
+                        <div>{genderIcon} {info.gender ? info.gender : "No gender provided"}</div>
+                    </div>
                     {!isFavorite ?
                     <div 
                     className="to-favorites" 
